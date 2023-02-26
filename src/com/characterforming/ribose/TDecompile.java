c55e069ae3797d8ab4890e5049d4e481ba0a7277
jrte <jrte.project@gmail.com>
2023-02-26 17:42:47 -0400

    Use Fibonacci to demonstrate decompilation output
    - no ci
    - in TDecompile javadoc

--- src/com/characterforming/ribose/TDecompile.java
+++ src/com/characterforming/ribose/TDecompile.java
@@ -43,105 +43,31 @@ import com.characterforming.ribose.base.ModelException;
  * </table>
  * <hr>
  * <pre>
- * LinuxKernel
+ * $ java -ea -cp jars/ribose-0.0.1.jar com.characterforming.ribose.TDecompile build/Test.model Fibonacci
+ * 
+ * Fibonacci
  * 
  * Input equivalents (equivalent: input...)
  * 
- *    0: k
- *    1: ^100
- *    2: ^102 ^103 ^104 ^105 ^106 ^107 ^108
- *    3: R
- *    4: 0 1 2 3 4 5 6 7 8 9
- *    5: .
- *    6: S
- *    7: N
- *    8: E
- *    9: ^0 ^1 ^2 ^3 ^4 ^5 ^6 ^7 ^8 ^9 ^b ^c ^d ^e ^f ^10 ^11 ^12 ^13 ^14 ^15 ^16 ^17 ^18 ^19 ^1a ^1b ^1c ^1d ^1e ^1f ^7f ^80 ... ^ff
- *   10: I
- *   11: =
- *   12: L
- *   13: ^a
- *   14: F G H J K Q V W X Y Z
- *   15: T
- *   16: P
- *   17: U
- *   18: D
- *   19: :
- *   20: O
- *   21: A
- *   22: ]
- *   23: ! " # $ % &amp; ' ( ) * + , - / ;  &lt; &gt; ? @ \ ^ _ ` { | } ~
- *   24: a b c d f g h i j m o p q s t u v w x y z
- *   25: ^20
- *   26: r
- *   27: ^101
- *   28: B
- *   29: C
- *   30: l
- *   31: n
- *   32: e
- *   33: [
- *   34: M
+ *    0: ^a
+ *    1: ^0 ^1 ^2 ^3 ^4 ^5 ^6 ^7 ^8 ^9 ^b ^c ^d ^e ^f ^10 ^11 ^12 ^13 ^14 ^15 ^16 ^17 ^18 ^19 ^1a ^1b ^1c ^1d ^1e ^1f ^20
+ *       ! " # $ % &amp; ' ( ) * + , - . / 1 2 3 4 5 6 7 8 9 : ; &lt; = &gt; ?
+ *       @ A B C D E F G H I J K L M N O P Q R S T U V W X Y Z [ \ ] ^ _
+ *       ` a b c d e f g h i j k l m n o p q r s t u v w x y z { | } ~ ^7f 
+ *       ^80 ^81 ^82 ^83 ^84 ^85 ^86 ^87 ^88 ^89 ^8a ^8b ^8c ^8d ^8e ^8f ... ^ff
+ *       ^100 ^102 ^104 ^105 ^106 ^107 ^108
+ *    2: ^103
+ *    3: ^101
+ *    4: 0
  * 
  * State transitions (from equivalent -&gt; to effect...)
  * 
- *   0  27 -&gt;   1 clear[0] select[6]
- *   1   1 -&gt;   2 1
- *   1   3 -&gt;   3 paste
- *   1   6 -&gt;   3 paste
- *   1   7 -&gt;   3 paste
- *   1   8 -&gt;   3 paste
- *   1  10 -&gt;   3 paste
- *   1  12 -&gt;   3 paste
- *   1  14 -&gt;   3 paste
- *   1  15 -&gt;   3 paste
- *   1  16 -&gt;   3 paste
- *   1  17 -&gt;   3 paste
- *   1  18 -&gt;   3 paste
- *   1  20 -&gt;   3 paste
- *   1  21 -&gt;   3 paste
- *   1  28 -&gt;   3 paste
- *   1  29 -&gt;   3 paste
- *   1  34 -&gt;   3 paste
- *   2   0 -&gt;   2 msum[1]
- *   2   3 -&gt;   2 msum[1]
- *   2   4 -&gt;   2 msum[1]
- *   2   5 -&gt;   2 msum[1]
- *   2   6 -&gt;   2 msum[1]
- *   2   7 -&gt;   2 msum[1]
- *   2   8 -&gt;   2 msum[1]
- *   2   9 -&gt;   2 msum[1]
- *   2  10 -&gt;   2 msum[1]
- *   2  11 -&gt;   2 msum[1]
- *   2  12 -&gt;   2 msum[1]
- *   2  13 -&gt;   1 clear[0] select[6]
- *   2  14 -&gt;   2 msum[1]
- *   2  15 -&gt;   2 msum[1]
- *   2  16 -&gt;   2 msum[1]
- *   2  17 -&gt;   2 msum[1]
- *   2  18 -&gt;   2 msum[1]
- *   2  19 -&gt;   2 msum[1]
- *   2  20 -&gt;   2 msum[1]
- *   2  21 -&gt;   2 msum[1]
- *   2  22 -&gt;   2 msum[1]
- *   2  23 -&gt;   2 msum[1]
- *   2  24 -&gt;   2 msum[1]
- *   2  25 -&gt;   2 msum[1]
- *   2  26 -&gt;   2 msum[1]
- *   2  28 -&gt;   2 msum[1]
- *   2  29 -&gt;   2 msum[1]
- *   2  30 -&gt;   2 msum[1]
- *   2  31 -&gt;   2 msum[1]
- *   2  32 -&gt;   2 msum[1]
- *   2  33 -&gt;   2 msum[1]
- *   2  34 -&gt;   2 msum[1]
- *   3   0 -&gt;   4 paste
- *   3   1 -&gt;   2 1
- *   3  24 -&gt;   4 paste
- *   3  26 -&gt;   4 paste
- *   3  30 -&gt;   4 paste
- *   3  31 -&gt;   4 paste
- *   3  32 -&gt;   4 paste
+ *   0   2 -&gt;   0 stop
+ *   0   3 -&gt;   1 clear[0]
+ *   1   0 -&gt;   0 1 paste signal[4]
+ *   1   4 -&gt;   2 select[3] paste[12]
+ *   2   0 -&gt;   0 1 paste signal[4]
+ *   2   4 -&gt;   2 select[4] cut[0] select[5] copy[0] select[3] cut[1]
  * </pre>
  */
 public class TDecompile {
